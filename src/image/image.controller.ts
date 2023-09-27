86f40dc35b1b8dcabfc741ba322705034bab947b
fabian <fabiancgraterol@gmail.com>
2023-09-27 11:43:47 -0400

    adding filter for file types

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -6,11 +6,12 @@ import {
   Get,
   Param,
   Res,
+  BadRequestException,
 } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 
 @Controller('image')
 export class ImageController {
@@ -28,10 +29,25 @@ export class ImageController {
         destination: './uploads',
         filename: function (req, file, callback) {
           const [name, ext] = file.originalname.split('.');
-          const newName = name.split(' ').join('') + Date.now().toString() +'.' + ext;
+          const newName =
+            name.split(' ').join('') + Date.now().toString() + '.' + ext;
           callback(null, newName);
         },
       }),
+      fileFilter(
+        req: Request,
+        file: {
+          mimetype: string;
+        },
+        callback: (error: Error | null, acceptFile: boolean) => void,
+      ) {
+        if (!file.mimetype.includes('image'))
+          return callback(
+            new BadRequestException('Provide a valid image'),
+            false,
+          );
+        return callback(null, true);
+      },
     }),
   )
   create(@UploadedFile() file: Express.Multer.File) {
